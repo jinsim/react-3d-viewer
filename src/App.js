6b8b769e468cd6c910abc9e676fc00c0a4c21793
deborah91298 <deborah91298@korea.ac.kr>
2022-08-22 19:10:11 +0700

    메인페이지 초기 틀

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import {
 } from "react-router-dom";
 import ThreeScene from './components/ThreeScene.js';
 import Main from './pages/Main.js';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <NavBar />
         <Routes>
-          {/* <Route path="/3d" exact element={<ThreeScene obj='Model3D/owl_1000.obj'/>} />  */}
-          <Route path="/" exact element={<Main />} /> 
+          <Route path="/3d" exact element={<ThreeScene obj='Model3D/owl_1000.obj'/>} /> 
+          <Route path="/" exact element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
